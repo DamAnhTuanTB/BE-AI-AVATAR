997c4f99c50bfd6724bcac9eda538bf251b1b181
trung <trungld@apero.vn>
2023-09-23 14:34:05 +0700

    add log

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -7,6 +7,8 @@ export class MailService {
   constructor(private mailerService: MailerService) {}
 
   async sendMail(payload: SendMail) {
+    console.log('USERNAME', process.env.MAIL_USER);
+    console.log('MAIL_PASSWORD', process.env.MAIL_PASSWORD);
     const { to, subject, template, context } = payload;
     try {
       await this.mailerService.sendMail({
